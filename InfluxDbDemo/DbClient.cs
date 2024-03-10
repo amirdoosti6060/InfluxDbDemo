ca44b9cd839652b7a5118360a900940f6f025198
Amir Doosti <amirdoosti@hotmail.com>
2024-03-10 11:49:02 +0400

    Add project files.

---
+++ InfluxDbDemo/DbClient.cs
@@ -0,0 +1,78 @@
+﻿using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Core;
+using InfluxDB.Client.Writes;
+
+namespace InfluxDbDemo
+{
+    public class DbClient: IDisposable
+    {
+        private readonly string _url;
+        private readonly string _token;
+        private readonly string _bucket;
+        private readonly string _organization;
+
+        private readonly InfluxDBClient _client;
+        private readonly Random _random;
+
+        public DbClient(string url, string token, string bucket, string organization) 
+        {
+            _url = url;
+            _token = token;
+            _bucket = bucket;
+            _organization = organization;
+
+            _random = new Random(DateTime.Now.Millisecond);
+
+            _client = new InfluxDBClient(_url, _token); 
+        }
+
+        public void InsertByPointData()
+        {
+            var temperature = (double)_random.Next(20, 40) + _random.NextDouble();
+
+            var point = PointData
+                            .Measurement("worldtemperature")
+                            .Tag("city", "Tehran")
+                            .Field("temperature", temperature);
+
+            using (var writeApi = _client.GetWriteApi())
+            {
+                writeApi.WritePoint(point: point, bucket: _bucket, org: _organization);
+            }
+
+            Console.WriteLine($"city: Tehran, Temperature:{temperature}");
+        }
+
+        public void InsertByLineProtocol()
+        {
+            var temperature = (double)_random.Next(20, 40) + _random.NextDouble();
+            string line = $"worldtemperature,city=London temperature={temperature}";
+
+            using (var writeApi = _client.GetWriteApi())
+            {
+                writeApi.WriteRecord(record: line, bucket: _bucket, org: _organization);
+            }
+
+            Console.WriteLine($"city: London, Temperature:{temperature}");
+        }
+
+        public async Task QueryByFlux()
+        {
+            var query = "from(bucket: \"DefaultBucket\") |> range(start: -10d) |> filter(fn: (r) => r._measurement == \"worldtemperature\") ";
+            var tables = await _client.GetQueryApi().QueryAsync(query, _organization);
+
+            Console.WriteLine("All world temperature data from 10 days ago until now:\n");
+            Console.WriteLine("Timestamp\t\tcity\ttemperature");
+            foreach (var record in tables.SelectMany(table => table.Records))
+            {
+                Console.WriteLine($"{record.GetTimeInDateTime()}\t{record.GetValueByKey("city")}\t{record.GetValueByKey("_value")}");
+            }
+        }
+
+        public void Dispose() 
+        { 
+            _client.Dispose();
+        }
+    }
+}
---
+++ InfluxDbDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using InfluxDbDemo;
+
+const string host = "http://localhost:8086";
+const string token = "FvmX6K-EsePzBlay8zPqgzTHmGOuDchSEK_SHIIWZKTD_-8cd0X7C8nc2aKvm9rm92xVixXrI0THqEtWaN7_-Q==";
+const string database = "DefaultBucket";
+const string organization = "DemoOrg";
+string[] commandList = {"P", "L", "Q", "X"};
+string? command;
+
+
+while(true)
+{
+    Console.Clear();
+    Console.WriteLine("InfluxDB Demo!");
+    Console.WriteLine("\n\nCommand: ");
+    Console.WriteLine("\tP=Insert By Pointdata");
+    Console.WriteLine("\tL=Insert By LineProtocol");
+    Console.WriteLine("\tQ=Query by Flux");
+    Console.WriteLine("\tX=Exit");
+    Console.Write(" > ");
+    command = Console.ReadKey().KeyChar.ToString().ToUpper();
+
+    if (commandList.Contains(command))
+        break;
+}
+Console.Write(" < \n\n");
+
+if (command != "X")
+{
+    var db = new DbClient(host, token, database, organization);
+    switch (command)
+    {
+        case "P":
+            Console.WriteLine("Insert 10 Points for Tehran");
+            for (int i = 0; i < 10; i++)
+            {
+                db.InsertByPointData();
+                await Task.Delay(1000);
+            }
+            break;
+        case "L":
+            Console.WriteLine("Insert 10 Points for London");
+            for (int i = 0; i < 10; i++)
+            {
+                db.InsertByLineProtocol();
+                await Task.Delay(1000);
+            }
+            break;
+        case "Q":
+            await db.QueryByFlux();
+            break;
+    }
+}
